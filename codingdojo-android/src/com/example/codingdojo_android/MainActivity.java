a2365da3cc87e516d6264617fe46bf2f92370401
Jhon_chavarria <Jhon.chavarria@gmail.com>
2013-07-10 15:43:33 -0600

    Haciendo el llamado a las diferentes actividades mediante los respectivos metodos

--- codingdojo-android/src/com/example/codingdojo_android/MainActivity.java
+++ codingdojo-android/src/com/example/codingdojo_android/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.codingdojo_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
-
+	private Intent intent;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,9 +20,15 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}	
+	  /** Read Comments  **/
 	  public void GoToRead(View v) {
+		 intent = new Intent(MainActivity.this,ReadActivity.class);
+		 startActivity(intent);
 	  }
 	  
+	  /** Send Form  **/
 	  public void GoToSend(View v) {
+		  intent = new Intent(MainActivity.this,SendActivity.class);
+		  startActivity(intent);
 	  }
 }
