caf12f959da875b25b6baa3c6e8fca7793928731
Jhon_chavarria <Jhon.chavarria@gmail.com>
2013-07-10 16:01:55 -0600

    Disenhando formulario para el envio de comentarios

--- codingdojo-android/src/com/example/codingdojo_android/SendActivity.java
+++ codingdojo-android/src/com/example/codingdojo_android/SendActivity.java
@@ -1,15 +1,23 @@
 package com.example.codingdojo_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class SendActivity extends Activity {
-
+	private EditText name,email,comment;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.send_activity);
+	}
+	
+	/** Send data  **/
+	public void SendData(View v) {
+		
 	}
 }
