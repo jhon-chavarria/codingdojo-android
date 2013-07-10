0763f1d18928540211a3f4bb0ade11a6baabd171
Jhon_chavarria <Jhon.chavarria@gmail.com>
2013-07-10 15:22:53 -0600

    Creando actividades para los dos eventos que habran(mandar y leer)

---
+++ codingdojo-android/src/com/example/codingdojo_android/SendActivity.java
@@ -0,0 +1,15 @@
+package com.example.codingdojo_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class SendActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.send_activity);
+	}
+}
