c070b4e3e5be1f98172a6d8e1f1a57ef58570a1b
Paulo McNally <paulo@mcnallydevelopers.com>
2013-07-10 16:42:03 -0600

    change localhost to 69.194.224.170

--- node/config.js
+++ node/config.js
@@ -3,5 +3,5 @@ mongodb.url = "mongodb://127.0.0.1/test";
 exports.mongodb = mongodb;
 
 var socket = {};
-socket.domain = "http://localhost";
+socket.domain = "http://69.194.224.170";
 exports.socket = socket;
