c1bd4bffc19ba19d85c2db7c1968d8c1f905a44b
Jhon_chavarria <Jhon.chavarria@gmail.com>
2013-07-10 16:25:32 -0600

    avanzando con el evento que enviara la data por post

--- codingdojo-android/src/com/example/codingdojo_android/SendActivity.java
+++ codingdojo-android/src/com/example/codingdojo_android/SendActivity.java
@@ -1,5 +1,17 @@
 package com.example.codingdojo_android;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +21,7 @@ import android.widget.EditText;
 
 public class SendActivity extends Activity {
 	private EditText name,email,comment;
+	private int responsehttp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +30,12 @@ public class SendActivity extends Activity {
 	}
 	
 	/** Send data  **/
-	public void SendData(View v) {
+	public void SendData(View v) throws ClientProtocolException {
 		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost("Url desde @String");
+        
+		// Add your data
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);	
 	}
 }
