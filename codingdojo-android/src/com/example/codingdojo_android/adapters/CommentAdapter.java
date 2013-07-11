3457bbef66652bbb41c545fe8cdf339885c4201e
Jhon_chavarria <Jhon.chavarria@gmail.com>
2013-07-11 13:28:36 -0600

    formateando la fecha

--- codingdojo-android/src/com/example/codingdojo_android/adapters/CommentAdapter.java
+++ codingdojo-android/src/com/example/codingdojo_android/adapters/CommentAdapter.java
@@ -1,5 +1,6 @@
 package com.example.codingdojo_android.adapters;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import com.example.codingdojo_android.entities.Comment;
 import com.example.codingdojo_android.R;
@@ -33,11 +34,36 @@ public class CommentAdapter extends ArrayAdapter<Comment> {
 	
     	TextView name=(TextView)row2.findViewById(R.id.name_list);
     	TextView text=(TextView)row2.findViewById(R.id.text_list);
+    	TextView date=(TextView)row2.findViewById(R.id.date);
+    	date.setText(GetDateFormat(c.getDate()));
     	name.setText(c.getName());
     	text.setText(c.getMessage());
 		return row2;
     
     }
+    
+    /**
+	 * Retorna la fecha en el formato deseado
+	 *
+	 */
+	public String GetDateFormat(String date) {
+			
+		String date1 = date;
+		String date2 = null;
+		SimpleDateFormat form = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date d1 = null;
+
+		try {
+			d1 = form.parse(date1);
+			SimpleDateFormat sdf = new SimpleDateFormat("EEE dd MMM");		    		
+			date2 = sdf.format(d1).toString();
+		} catch (java.text.ParseException e) {
+	                // TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			 	
+		return date2;
+	}
     public List<Comment> getItems() {	
 		return items;
 	}
