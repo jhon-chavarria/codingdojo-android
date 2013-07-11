e90964f156f70fa392218faaaac280afb0508ac9
Paulo McNally <paulo@mcnallydevelopers.com>
2013-07-11 11:38:23 -0600

    change domain to nodejs.clov3r.net

--- node/config.js
+++ node/config.js
@@ -3,5 +3,5 @@ mongodb.url = "mongodb://127.0.0.1/test";
 exports.mongodb = mongodb;
 
 var socket = {};
-socket.domain = "http://jhon.clov3r.net";
+socket.domain = "http://nodejs.clov3r.net";
 exports.socket = socket;
